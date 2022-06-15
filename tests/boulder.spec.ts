1aa5e5572f87d63c8eb0dfc357835655d5b5dabb
Jacob Korf <korfj921921@gmail.com>
2022-06-15 09:14:46 -0600

    Finds Sitemap, file for links, all tied together

--- tests/boulder.spec.ts
+++ tests/boulder.spec.ts
@@ -1,24 +1,47 @@
+import { test, expect, Page } from "@playwright/test";
+import * as fs from "fs";
+import * as path from "path";
+import {
+  injectAxe,
+  checkA11y,
+  getViolations,
+  reportViolations,
+} from "axe-playwright";
 
-import { test, expect, Page } from '@playwright/test';
-const urlList = [
-  'https://www.google.com',
-  'https://catalog.colorado.edu/continuing-education/'
-];
-for( let url of urlList) {
+var links = fs
+  .readFileSync(path.resolve(__dirname, "foo.txt"))
+  .toString()
+  .split("\n")
+  .map((e) => e.trim());
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto(url);
-      });
-      
-test('basic test ' + url, async ({ page }) => {
-    await page.goto(url);
-    const logo = page.locator('.page-title');
-    await expect(logo).toHaveText('Continuing Education Catalog');
+for (const link of links) {
+  test("basic test " + link, async ({ page }) => {
+    await page.goto(link);
+    const logo = page.locator(".page-title");
+    await expect(logo).toHaveText("Continuing Education Catalog");
   });
 
-  test('basic test fail ' + url, async ({ page }) => {
-    await page.goto(url);
-    const logo = page.locator('.page-title');
-    await expect(logo).toHaveText('Contining Education Catalog');
+  test("basic test fail " + link, async ({ page }) => {
+    await page.goto(link);
+    const logo = page.locator(".page-title");
+    await expect(logo).toHaveText("Contining Education Catalog");
+  });
+
+  test("simple accessibility run " + link, async ({ page }) => {
+    await page.goto(link);
+    await injectAxe(page);
+    await checkA11y(page);
+  });
+
+  test("test server " + link, async ({ page }) => {
+    const url = new URL(link);
+    const sitemap = url.protocol + "//" + url.hostname + "/sitemap.xml";
+
+    //await page.goto("https://playwright.dev/docs/intro");
+    const [response] = await Promise.all([
+      page.waitForResponse((res) => res.status() == 200),
+      await page.goto(sitemap),
+      //await page.locator(".navbar__brand").click,
+    ]);
   });
 }
