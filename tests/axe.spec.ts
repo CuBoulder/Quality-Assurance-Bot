9e49f6c3e6529a73e9237bde758dcd58948a8b2a
Jacob Korf <korfj921921@gmail.com>
2022-10-25 21:02:32 -0600

    2 vers. of the tests, almost complete sitemapper

---
+++ tests/axe.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect, Page } from "@playwright/test";
+import * as fs from "fs";
+import * as path from "path";
+import { HomePage } from "../pages/homepage.page";
+import {
+  injectAxe,
+  checkA11y,
+  getViolations,
+  reportViolations,
+} from "axe-playwright";
+var links = fs
+  .readFileSync(path.resolve(__dirname, "foo.txt"))
+  .toString()
+  .split("\n")
+  .map((e) => e.trim());
+
+for (const link of links) {
+  test("basic test " + link, async ({ page }) => {
+    await page.goto(link);
+    const logo = page.locator(".page-title");
+    await expect(logo).toHaveText("Continuing Education Catalog");
+  });
+
+  test("basic test fail " + link, async ({ page }) => {
+    await page.goto(link);
+    const logo = page.locator(".page-title");
+    await expect(logo).toHaveText("Contining Education Catalog");
+  });
+
+  test("simple accessibility run " + link, async ({ page }) => {
+    await page.goto(link);
+    await injectAxe(page);
+    await checkA11y(page);
+  });
+
+  test("test server " + link, async ({ page }) => {
+    const url = new URL(link);
+    const sitemap = url.protocol + "//" + url.hostname + "/sitemap.xml";
+
+    //await page.goto("https://playwright.dev/docs/intro");
+    const [response] = await Promise.all([
+      page.waitForResponse((res) => res.status() == 200),
+      await page.goto(sitemap),
+      //await page.locator(".navbar__brand").click,
+    ]);
+  });
+
+  test("test live " + link, async ({ page }) => {
+
+    //await page.goto("https://playwright.dev/docs/intro");
+    const [response] = await Promise.all([
+      page.waitForResponse((res) => res.status() == 200),
+      await page.goto(link),
+      //await page.locator(".navbar__brand").click,
+    ]) ;
+  });
+}
--- tests/boulder.spec.ts
+++ tests/boulder.spec.ts
@@ -1,47 +1,44 @@
 import { test, expect, Page } from "@playwright/test";
 import * as fs from "fs";
 import * as path from "path";
+import { HomePage } from "../pages/homepage.page";
 import {
   injectAxe,
   checkA11y,
   getViolations,
   reportViolations,
 } from "axe-playwright";
+const DOTENV = require('dotenv').config();
+const Sitemapper = require("sitemapper");
+var links = ["google.com"];
+console.log(process.env.XML_FILE)
+var promise = new Promise(function (resolve, reject) {
+  links = ["https://www.colorado.edu/src/sitemap.xml"];
 
-var links = fs
-  .readFileSync(path.resolve(__dirname, "foo.txt"))
-  .toString()
-  .split("\n")
-  .map((e) => e.trim());
+  const sitemapper = new Sitemapper();
+  sitemapper.timeout = 5000;
+  sitemapper
+    .fetch(process.env.XML_FILE)
+    .then(({ url, sites }) => {
+      console.log(`url:${url}`, "lol:", sites);
+    })
 
-for (const link of links) {
-  test("basic test " + link, async ({ page }) => {
-    await page.goto(link);
-    const logo = page.locator(".page-title");
-    await expect(logo).toHaveText("Continuing Education Catalog");
+    .catch((error) => console.log(error));
+
+
+  resolve(links);
+});
+
+promise
+  .then(function () {
+    for (const link of links) {
+      test("basic test " + link, async ({ page }) => {
+        await page.goto(link);
+        const logo = page.locator(".sitename span");
+        await expect(logo).toHaveText("Physics");
+      });
+    }
+  })
+  .catch(function () {
+    console.log("Some error has occurred");
   });
-
-  test("basic test fail " + link, async ({ page }) => {
-    await page.goto(link);
-    const logo = page.locator(".page-title");
-    await expect(logo).toHaveText("Contining Education Catalog");
-  });
-
-  test("simple accessibility run " + link, async ({ page }) => {
-    await page.goto(link);
-    await injectAxe(page);
-    await checkA11y(page);
-  });
-
-  test("test server " + link, async ({ page }) => {
-    const url = new URL(link);
-    const sitemap = url.protocol + "//" + url.hostname + "/sitemap.xml";
-
-    //await page.goto("https://playwright.dev/docs/intro");
-    const [response] = await Promise.all([
-      page.waitForResponse((res) => res.status() == 200),
-      await page.goto(sitemap),
-      //await page.locator(".navbar__brand").click,
-    ]);
-  });
-}
